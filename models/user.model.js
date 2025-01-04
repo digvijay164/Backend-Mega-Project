cbf912c5c3bc9ee5739bf1bebbb39238aad8372b
digvijay164 <digvijaydesai164@gmail.com>
2025-01-04 10:35:24 +0530

    setup cloudinary and multer

--- models/user.model.js
+++ models/user.model.js
@@ -82,9 +82,9 @@ userSchema.methods.generateRefreshToken = function(){
         {
             _id: this._id
         },
-        process.env.REFRESH_TOKEN_EXPIRY,
+        process.env.REFRESH_TOKEN_SECRET,
         {
-            expiresIn: process.env.REFRESH _TOKEN_EXPIRY
+            expiresIn: process.env.REFRESH_TOKEN_EXPIRY
         }
     )
 };
---
+++ utils/cloudinary.js
@@ -0,0 +1,28 @@
+import { v2 as cloudinary } from "cloudinary";
+import fs from 'fs';
+   // Configuration
+   cloudinary.config({ 
+    cloud_name: process.env.CLOUDINARY_NAME, 
+    api_key: process.env.CLOUDINARY_API_KEY, 
+    api_secret: process.CLOUDINARY_env.API_SECRET 
+});
+
+const uploadCloudinary = async (localFilePath)=>{
+    try{
+        if(!localFilePath) return null;
+
+        // Upload file on cloudinary
+        const response = await cloudinary.uploader.upload(localFilePath, {
+            resource_type: "auto"
+        })
+        // File has been uploaded successfully
+        console.log(`file has been uploaded successfully ${response.url}`);
+        
+        return response;
+        
+    }catch(err){
+        fs.unlinkSync(localFilePath) // remove the locally saved file as the upload operation got failed
+        console.log(`file upload failed ERROR : ${err}`);
+        return null;
+    } 
+}
